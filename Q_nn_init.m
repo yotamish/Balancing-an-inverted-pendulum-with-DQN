65139bbca8c048e4ad265289aa6857fef4d1b979
Yotam Ishay <yotamish@gmail.com>
2017-10-10 14:58:17 +0300

    Update Q_nn_init.m

--- Q_nn_init.m
+++ Q_nn_init.m
@@ -1,14 +1,14 @@
 function [ net_Q ] = Q_nn_init( Q_NET_SIZES,STATE_SIZE,ACTION_SIZE )
 %Q_NN_INIT 
 %   initialize 2-hidden-layer Q neural network for the DQN algorithm
- 
+
 % load Q
 % net_Q=Q;
- 
+
 %WeightsPerLayer is the number of neurons in our 2 hidden layer nn
 %********************************define and init Q nn********************
 net_Q=feedforwardnet(Q_NET_SIZES);
- 
+
 % X=[1,0.5,-1,-0.5,1;1,-1,1,0.5,1;1,0.5,-1,-0.5,1;1,0.5,-1,-0.5,1;1,0.5,-1,-0.5,1];   %X is the input and determines the input dimension of the nn
 X=rand([STATE_SIZE,STATE_SIZE]);
 % T=[1,0,1,0,1;0,1,0,1,0;1,0,1,0,1;1,0,1,0,1;1,0,1,0,1];  %This is for 5 actions 
@@ -16,7 +16,7 @@ T=rand([ACTION_SIZE,STATE_SIZE]);
 % T=[1,0,1,0,1;0,1,0,1,0;1,0,1,0,1];  %This is for 3 actions  %T is the output and determines the output dimension of the nn.
 % FIXME: I tried to use one hot vectors for classification but It doesn't seem to
 % work because I'm getting bad outputs
- 
+
 % FIXME: initialize smarter (X,T doesn't really make sense)
 net_Q.trainFcn = 'traingdm';          %Gradient Descent Backpropagation
 net_Q.trainParam.epochs = 1;           %1 epoch becuase we are batch-training
@@ -28,6 +28,8 @@ net_Q.performFcn='mse';             %mean square error performance function
 %net_Q.performFcn='crossentropy';     %mean square error performance function
 init(net_Q);                        %initialize net
 net_Q=train(net_Q,X,T);
- 
- 
+
+
 end
+
+
